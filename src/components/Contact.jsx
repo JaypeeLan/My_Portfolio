0907a02382cf9e142f5706e5a19f4587c0f74c63
JohnPaul Laniran <jplaniran01@gmail.com>
2022-09-15 11:32:00 +0100

    updated contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
 import { BsGithub, BsLinkedin } from 'react-icons/bs'
-import { Form, Input, TextArea, Button, Select } from 'semantic-ui-react'
+// import { Form, Input, TextArea, Button, Select } from 'semantic-ui-react'
 
 const Contact = () => {
   return (
     <section id="contact">
       <h2>Say Hello</h2>
-      <Form>
-        <Form.Field
-          id="form-input-control-error-email"
-          control={Input}
-          placeholder="Email: "
-        />
-        <Form.Field
-          id="form-textarea-control-opinion"
-          control={TextArea}
-          placeholder="Message"
-          rows="9"
-        />
 
-        <Form.Field
-          id="form-button-control-public"
-          control={Button}
-          content="Submit"
-        />
-      </Form>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="email">
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+
+        <p>
+          <label htmlFor="Message">
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
 
       <div className="social_icons">
         <a
@@ -32,7 +35,7 @@ const Contact = () => {
           target="_blank"
           className="socials"
         >
-          <BsGithub size={35} color="green"/>
+          <BsGithub size={35} color="green" />
         </a>
 
         <a
@@ -40,7 +43,7 @@ const Contact = () => {
           target="_blank"
           className="socials"
         >
-          <BsLinkedin  size={35} color="blue"/>
+          <BsLinkedin size={35} color="blue" />
         </a>
       </div>
     </section>
